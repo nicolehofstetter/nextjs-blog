79addb12eeaffecff7f316bc670b25fa5cdef6e9
Nicole Hofstetter <nicole.hofstetter.work@gmail.com>
2023-01-10 15:54:04 +0100

    Next Js tutorial to styling section

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Link from "next/link";
+
+export default function Home({
+                               allPostsData
+                             }: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
+  return (
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+          <p>Hello, I'm Nicole.</p>
+          <p>
+            (This is a sample website - you’ll be building a site like this in{' '}
+            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          </p>
+        </section>
+          <section>
+              <Link href={"posts/first-post"}>First post is here</Link>
+          </section>
+      </Layout>
+  )
+}
