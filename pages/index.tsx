6895dc75532d38618d3fdec26a53e61ff25c8c46
Nicole Hofstetter <nicole.hofstetter.work@gmail.com>
2023-01-10 16:03:44 +0100

    Change from css to scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import utilStyles from '../styles/utils.module.scss'
 import Link from "next/link";
 
 export default function Home({
